7c66f196a1569dea0f3a36d78a0dbdff2c3af80b
sillymoose <davidnmfarrell@gmail.com>
2014-09-19 00:05:06 -0400

    initial release

---
+++ lib/WWW/hmaip.pm
@@ -0,0 +1,59 @@
+use strict;
+use warnings;
+package WWW::hmaip;
+
+use HTTP::Tiny;
+use 5.008;
+
+# ABSTRACT: Returns your ip address using L<http://geoip.hidemyass.com/ip/>
+
+=head1 SYNOPSIS
+
+    use WWW::hmaip;
+
+    my $ip = get_ip(); # 54.123.84.6
+
+=head1 EXPORTS
+
+Exports the C<get_ip> function.
+
+=cut
+
+BEGIN {
+    require Exporter;
+    use base 'Exporter';
+    our @EXPORT = 'get_ip';
+    our @EXPORT_OK = ();
+}
+
+=head1 FUNCTIONS
+
+=head2 get_ip
+
+Returns your ip address, using L<http://geoip.hidemyass.com/ip/>.
+
+    use WWW::hmaip;
+
+    my $ip = get_ip();
+
+=cut
+
+sub get_ip {
+    my $response = HTTP::Tiny->new->get('http://geoip.hidemyass.com/ip/');
+    die join(' ', 'Error fetching ip: ',
+                  ($response->{status} or ''),
+                  ($response->{reason} or '')) unless $response->{success};
+    my $ip = $response->{content};
+    #chomp $ip;
+    $ip;
+}
+
+=head1 SEE ALSO
+
+L<WWW::curlmyip> - another module that returns your ip address
+
+L<WWW::ipinfo> - a module that returns ip address and geolocation data
+
+=cut
+
+1;
